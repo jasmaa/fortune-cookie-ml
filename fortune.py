0bc4083a1a54e68085bcc879ef4ff07137693bd1
jasmaa <jasonmaa3955@gmail.com>
2019-03-29 16:13:22 -0400

    Implement model, start training

--- fortune.py
+++ fortune.py
@@ -4,11 +4,37 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 import json
+import random
 import re
 
+# === MODELS ===
+class FortuneIdentifier(nn.Module):
+    """Fortune cookie identifier
+    """
+    
+    def __init__(self, embedding_dim, hidden_dim, vocab_size, max_size):
+        super(FortuneIdentifier, self).__init__()
+        self.embedding_dim = embedding_dim
+        self.hidden_dim = hidden_dim
+        self.max_size = max_size
 
-# === Preprocess ===
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim)
 
+        self.connected = nn.Sequential(
+            nn.Linear(hidden_dim * max_size, 16),
+            nn.ReLU(),
+            nn.Linear(16, 2),
+            nn.Softmax(),
+        )
+
+    def forward(self, x):
+        embeds = self.embedding(x)
+        out, _ = self.lstm(embeds.view(len(x), 1, -1))
+        scores = self.connected(out.view(1, -1))
+        return scores.squeeze()
+
+# === PRE-PROCCESS ===
 class Vocab():
     """Vocab to encode sentences to tensors
     """
@@ -43,37 +69,92 @@ class Vocab():
 
         idx_list = [0] * max_size
         i = 0
-        for i, word in enumerate(sentence):
+        for word in sentence:
             try:
                 idx_list[i] = self.word2idx[word]
             except KeyError:
-                idx_list[i] = -1 #random.randint(3, self.size-1)
+                idx_list[i] = random.randint(3, self.size-1)
+            i += 1
         idx_list[i] = self.word2idx["<EOS>"]
 
-        return torch.FloatTensor(idx_list)
-        
+        return torch.LongTensor(idx_list)
+
+
+def clean_sentence(data):
+    """Cleans sentence for indexing
+    """
+    data = data.split(" ")
+    data = list(map(lambda x: x.lower(), data))
+    data = list(map(lambda x: re.sub(r'\W+', '', x), data))
+    return data
 
 # load vocab
 vocab = Vocab()
 vocab.load_vocab("words_alpha.txt")
-    
-# load data
-processed_fortunes = []
+
+max_len = -1
+
+# load fortunes
+fortunes = []
 with open("fortunes.json", "r") as f:
-    fortunes = json.loads(f.read())
-    max_len = -1
-    
-    for key in fortunes:
-        data = fortunes[key].split(" ")
-        data = list(map(lambda x: x.lower(), data))
-        data = list(map(lambda x: re.sub(r'\W+', '', x), data))
+    data = json.loads(f.read())
+    for key in data:
+        s = clean_sentence(data[key])
+        fortunes.append(s)
+        if len(s) > max_len:
+            max_len = len(s)
+            
+# load non-fortunes
+nonfortunes = []
+with open("nonfortunes.json", "r") as f:
+    data = json.loads(f.read())
+    for key in data:
+        s = clean_sentence(data[key])
+        nonfortunes.append(s)
+        if len(s) > max_len:
+            max_len = len(s)
 
-        processed_fortunes.append(data)
-
-        if len(data) > max_len:
-            max_len = len(data)
 
 # convert to tensor
-sentence_tensors = []
-for s in processed_fortunes:
-    sentence_tensors.append(vocab.encode(s, max_len+1))
+all_data = []
+for s in fortunes:
+    all_data.append((vocab.encode(s, max_len+1), torch.tensor([1., 0])))
+for s in nonfortunes:
+    all_data.append((vocab.encode(s, max_len+1), torch.tensor([0., 1])))
+
+# partition training and test data
+#TODO
+train_data = all_data
+
+# === TRAIN ===
+HIDDEN_DIM = 64
+EMBEDDING_DIM = 64
+EPOCHS = 100
+BATCH_SIZE = 20
+
+model = FortuneIdentifier(EMBEDDING_DIM, HIDDEN_DIM, vocab.size, max_len+1)
+loss_f = nn.BCELoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+
+loss_list = []
+
+print("Start training...")
+for epoch in range(EPOCHS):
+    avg_loss = 0
+    for inp, target in random.sample(train_data, BATCH_SIZE):
+        model.zero_grad()
+
+        scores = model(inp)
+
+        loss = loss_f(scores, target)
+        avg_loss += loss
+        loss.backward()
+        optimizer.step()
+
+    loss_list.append(avg_loss)
+    print(f"Epoch {epoch}\tAvg Loss: {avg_loss / BATCH_SIZE}")
+
+
+# === TEST ===
+with torch.no_grad():
+    pass
